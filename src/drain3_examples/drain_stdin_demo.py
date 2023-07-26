3ba5da49b3b5fbbf7c100d87c49b275f4dc59b04
frsot373 <38676098+frsot373@users.noreply.github.com>
2023-07-26 09:20:32 +0000

    dev

--- src/drain3_examples/drain_stdin_demo.py
+++ src/drain3_examples/drain_stdin_demo.py
@@ -9,47 +9,22 @@ from drain3 import TemplateMiner
 from drain3.template_miner_config import TemplateMinerConfig
 from src.common_config import CONFIG_DIR_PATH, USE_OLD_FUNCTION_EXTRACT_PARAMETER, TOKEN_LIST_KEY, \
     TEMPLATE_MINED_KEY, LOG_TEMPLATE_TOKENS_KEY, DEFAULT_STR_VALUE
+from drain3.file_persistence import FilePersistence
 
-# persistence_type = "NONE"
-# persistence_type = "REDIS"
-# persistence_type = "KAFKA"
-persistence_type = "FILE"
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
 
-if persistence_type == "KAFKA":
-    from drain3.kafka_persistence import KafkaPersistence
-
-    persistence = KafkaPersistence("drain3_state", bootstrap_servers="localhost:9092")
-
-elif persistence_type == "FILE":
-    from drain3.file_persistence import FilePersistence
-
-    # persistence = FilePersistence("drain3_state.bin")
-    drain3_state_bin_file_path = os.path.join(CONFIG_DIR_PATH, "drain3_state.bin")
-    persistence = FilePersistence(drain3_state_bin_file_path)
-
-elif persistence_type == "REDIS":
-    from drain3.redis_persistence import RedisPersistence
-
-    persistence = RedisPersistence(redis_host='',
-                                   redis_port=25061,
-                                   redis_db=0,
-                                   redis_pass='',
-                                   is_ssl=True,
-                                   redis_key="drain3_state_key")
-else:
-    persistence = None
+drain3_state_bin_file_path = os.path.join(CONFIG_DIR_PATH, "drain3_state.bin")
+persistence = FilePersistence(drain3_state_bin_file_path)
 
 config = TemplateMinerConfig()
-# config.load(dirname(__file__) + "/drain3.ini")
 drain3_ini_file_path = os.path.join(CONFIG_DIR_PATH, "drain3.ini")
 config.load(drain3_ini_file_path)
 config.profiling_enabled = False
 
 template_miner = TemplateMiner(persistence, config)
-print(f"Drain3 started with '{persistence_type}' persistence")
 print(f"{len(config.masking_instructions)} masking instructions are in use")
 print(f"Starting training mode. Reading from std-in ('q' to finish)")  # yd。利用输入的一条条日志，训练得到模板
 while True:
